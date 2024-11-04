85cc49ccd154ed11059008fc68c43e1e7fafc539
iwanlenin <j.geberlin@gmail.com>
2024-11-04 14:35:05 +0100

    SPEN-45 : Downgrade all Nuget packages to last stable version SPEN-22 : View to ViewModel connection SPEN-33 : BasViewModel

--- SpendWise/AppShell.xaml.cs
+++ SpendWise/AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace SpendWise
+﻿using SpendWise.Views;
+
+namespace SpendWise
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(AddExpensePage), typeof(AddExpensePage));
         }
     }
 }
